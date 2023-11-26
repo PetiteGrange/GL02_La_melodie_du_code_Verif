6dd3dcdcbab6bdb4e8312043ce7de7392d8800ca
Aurélien Juillé <aurelien.juille@utt.fr>
2023-11-27 00:35:31 +0100

    started GiftParser.js (tokenize and parse methods)

--- GiftParser.js
+++ GiftParser.js
@@ -1,1 +1,37 @@
-import Q from './Question';
+const Q = require('./Question.js')
+
+
+class GiftParser {
+    constructor(sTokenize) {
+        this.parsedQuestions = []
+        this.showTokenize = sTokenize
+        this.errorCount = 0
+    }
+
+    tokenize(data) {
+        var separator = /\r\n\s*\r\n+/
+		data = data.split(separator)
+		data = data.filter((val, idx) => val.trim() !== '' && val.includes('{') && val.includes('}'))
+		var questions = []
+        data.forEach(element => {
+            var lines = element.split(/\r?\n|\r/g)
+            lines = lines.filter(line => !line.trim().startsWith("//"))
+            var question = lines.join("")
+            questions.push(question.replace(/\r?\n|\r/g, ""))
+        });
+        return questions
+    }
+
+    parse(data) {
+        var tData = this.tokenize(data)
+        if (this.showTokenize) {
+			console.log(tData)
+		}
+    }
+
+    listQuestions(input) {
+        
+    }
+}
+
+module.exports = GiftParser;
