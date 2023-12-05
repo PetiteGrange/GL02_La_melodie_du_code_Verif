ed9a135cafead5e418c3018232cdbb11830dbb8c
Laparhasard <marian.vigneron@free.fr>
2023-12-05 12:52:20 +0100

    Modif askQuestionType

--- search.js
+++ search.js
@@ -9,8 +9,8 @@ const mainModule = require('./main.js');
 
 const Question = require('./Question.js');
 const filterQuestions = require('./filterQuestions');
-
-const { askQuestionType } = require('./askQuestionType');
+const QT = require('./QuestionType.js')
+const askForFileTypes = require('./askQuestionType');
 
 
 program
@@ -19,10 +19,13 @@ program
     .option('-n, --word <word>', 'le nom du fichier contient "word"')
     .option('-c, --expression <expresssion>', "le fichier que l'on veut afficher contient 'expression'")
     .action(({args, options, logger}) =>{
+        askForFileTypes((selectedTypes) => {
+            console.log('Types de fichiers sélectionnés dans app.js :', selectedTypes);
+        });
         if (args.name) {
             args.name.forEach(filename => {  //pour chaque fichier dont le nom a été entré par l'utilisateur
                 const filePath = path.join('data', filename);  //création du chemin
-                fs.readFile(filePath, 'utf-8', (err, content) => {  //lecture du fichier
+                fs.readFileSync(filePath, 'utf-8', (err, content) => {  //lecture du fichier
                     if (err) {
                         return console.log('Unable to scan file ' + filename + ': ' + err + '\n');
                     }
