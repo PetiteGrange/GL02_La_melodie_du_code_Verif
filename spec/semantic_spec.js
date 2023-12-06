47aa8ca81018a33134e502a1d3dc853a23e7ffe2
Ambrine <mbrnbencheikh@gmail.com>
2023-12-06 11:20:11 +0100

    Ajout de jasmine dans le projet et amélioration de choixQuestions.js

--- spec/semantic_spec.js
+++ spec/semantic_spec.js
@@ -4,31 +4,29 @@ describe("Program Semantic testing of Question", function(){
 
 	beforeAll(function() {
 
-		this.q = new Q;
+		this.q = new Q('EM U4 p33 Review 4.6 for the first time','[html]I had never seen a glacier before I went to Norway <br><b>I</b><br> When I xas in Norway, ->1<- the first time.',[ 'QUESTION_TEXT_A_TROUS' ],[ 'réponse' ], false);
 
 	});
 
-	xit("can create a new POI", function(){
+	it("peut créer un nouvel objet question à partir de données GIFT", function(){
 
-		expect(this.p).toBeDefined();
+		expect(this.q).toBeDefined();
 		// toBe is === on simple values
-		expect(this.p.name).toBe("Café d'Albert");
-		expect(this.p).toEqual(jasmine.objectContaining({name: "Café d'Albert"}));
+		expect(this.q.title).toBe('EM U4 p33 Review 4.6 for the first time');
+		expect(this.q).toEqual(jasmine.objectContaining({title: 'EM U4 p33 Review 4.6 for the first time'}));
+
+		expect(this.q.text).toBe('[html]I had never seen a glacier before I went to Norway <br><b>I</b><br> When I xas in Norway, ->1<- the first time.');
+		expect(this.q).toEqual(jasmine.objectContaining({text: '[html]I had never seen a glacier before I went to Norway <br><b>I</b><br> When I xas in Norway, ->1<- the first time.'}));
+
+		expect(this.q.type).toEqual([ 'QUESTION_TEXT_A_TROUS' ]);
+		expect(this.q).toEqual(jasmine.objectContaining({type: [ 'QUESTION_TEXT_A_TROUS' ]}));
+
+		expect(this.q.answer).toEqual([ 'réponse' ]);
+		expect(this.q).toEqual(jasmine.objectContaining({answer: [ 'réponse' ]}));
+
+		expect(this.q.partialCredit).toBe(false);
+		expect(this.q).toEqual(jasmine.objectContaining({partialCredit: false}));
 
 	});
-
-	xit("can add a new ranking", function(){
-
-		this.p.addRating(2);
-		// toEqual is === on complex values - deepEquality
-		expect(this.p.ratings).toEqual([1,3,2,2]);
-
-	});
-
-	xit ("can compute average ratings", function(){
-
-		expect(this.p.averageRatings()).toBe(2);
-	});
-
 
 });
